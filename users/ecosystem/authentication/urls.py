43ada1a08f75e46241f4a76b4a44876debe1b46b
FoxBeCoding <fox@foxbecoding.com>
2023-07-05 11:57:49 -0500

    updated urls

--- users/ecosystem/authentication/urls.py
+++ users/ecosystem/authentication/urls.py
@@ -2,6 +2,6 @@ from rest_framework.routers import DefaultRouter
 from users.ecosystem.authentication.views import *
 
 router = DefaultRouter()
-router.register(r"auth-log-in", AccountLogInViewSet, basename="account-log-in")
-router.register(r"auth-log-out", AccountLogOutViewSet, basename="account-log-out")
+router.register(r"auth-log-in", UserAuthLogInViewSet, basename="account-log-in")
+router.register(r"auth-log-out", UserAuthLogOutViewSet, basename="account-log-out")
 urlpatterns = router.urls
