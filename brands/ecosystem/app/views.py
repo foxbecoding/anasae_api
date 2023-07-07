befa8a4b77d111155c074744bafd485735aa643c
FoxBeCoding <fox@foxbecoding.com>
2023-07-07 17:58:45 -0500

    updated views

--- brands/ecosystem/app/views.py
+++ brands/ecosystem/app/views.py
@@ -9,3 +9,23 @@ from brands.serializers import *
 from brands.permissions import *
 from brands.ecosystem.methods import *
 from pprint import pprint
+
+class UserViewSet(viewsets.ViewSet):
+    def get_permissions(self):
+        permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+
+    @method_decorator(csrf_protect)
+    def create(self, request):
+        # Create_User_Serializer = CreateUserSerializer(data=request.data, context={'request': request})
+        # if not Create_User_Serializer.is_valid():
+        #     return Response(Create_User_Serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
+        
+        # User_Instance = Create_User_Serializer.validated_data['user']
+        # data = get_user_data(User_Instance)
+        return Response(None, status=status.HTTP_201_CREATED)
+
+    def retrieve(self, request, pk=None):
+        # self.check_object_permissions(request=request, obj={'user_pk': pk})
+        # data = get_user_data(request.user)
+        return Response(None, status=status.HTTP_200_OK)
