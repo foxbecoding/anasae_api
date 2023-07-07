fcea188c78e273bca52894ced9104e82d3e3c8ce
FoxBeCoding <fox@foxbecoding.com>
2023-07-07 18:29:35 -0500

    updated serializers

---
+++ brands/serializers.py
@@ -0,0 +1,72 @@
+from rest_framework import serializers
+from brands.models import *
+from utils.helpers import create_uid
+from PIL import Image
+import stripe, requests, os, calendar, time
+
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = [
+            'pk',
+            'uid',
+            'name',
+            'bio',
+            'stripe_account_id'
+            'owners',
+            'logo'
+        ]
+
+class EditBrandSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Brand
+        fields = [
+            'name',
+            'bio',
+        ]
+
+class CreateBrandSerializer(serializers.ModelSerializer):
+  
+    class Meta:
+        model = Brand
+        fields = [
+            'name',
+            'bio',
+        ]
+
+    def validate(self, attrs):
+        request = self.context['request']
+
+        Stripe_Account = stripe.Account.create(
+            type="custom",
+            country="US",
+            email=request.user.email,
+            capabilities={
+                "card_payments": {"requested": True},
+                "transfers": {"requested": True},
+            }
+        )
+
+        Brand_Instance = Brand.objects.create(
+            user = request.user,
+            uid = create_uid('b-'),
+            stripe_account_id = Stripe_Account.id,
+            name = attrs.get('name'),
+            bio = attrs.get('bio')
+        )
+        Brand_Instance.save()
+
+        Brand_Owner_Instance = BrandOwner.objects.create(
+            brand = Brand_Instance,
+            user = request.user
+        )
+        Brand_Owner_Instance.save()
+
+        stripe.Account.modify(
+            Stripe_Account.id,
+            metadata={ "brand_id": Brand_Instance.id },
+        )
+        
+        attrs['brand'] = Brand_Instance
+        return attrs
--- users/serializers.py
+++ users/serializers.py
@@ -3,7 +3,6 @@ from rest_framework import serializers
 from users.models import *
 from utils.helpers import create_uid
 from PIL import Image
-from utils.helpers import create_uid
 import stripe, requests, os, calendar, time
 
 env = os.getenv
