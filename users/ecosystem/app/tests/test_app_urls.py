d838bd45ce6f9e49cf888b285667927b5788e14e
FoxBeCoding <fox@foxbecoding.com>
2023-07-06 21:01:42 -0500

    updated test urls

--- users/ecosystem/app/tests/test_app_urls.py
+++ users/ecosystem/app/tests/test_app_urls.py
@@ -16,3 +16,13 @@ class TestUserImageUrls(SimpleTestCase):
     def test_user_image_list_url_resolves(self):
         url = reverse('user-image-list')
         self.assertEqual(resolve(url).view_name, 'user-image-list')
+
+class TestUserAddressUrls(SimpleTestCase):
+
+    def test_user_address_list_url_resolves(self):
+        url = reverse('user-address-list')
+        self.assertEqual(resolve(url).view_name, 'user-address-list')
+    
+    def test_user_address_detail_url_resolves(self):
+        url = reverse('user-address-detail', kwargs={'pk': 1})
+        self.assertEqual(resolve(url).view_name, 'user-address-detail')
