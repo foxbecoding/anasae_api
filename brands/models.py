891d1d16f5328c380ba02eef16793f4c96d1555f
FoxBeCoding <fox@foxbecoding.com>
2023-07-08 10:39:28 -0500

    updated models

--- brands/models.py
+++ brands/models.py
@@ -21,7 +21,11 @@ class BrandLogo(models.Model):
 class BrandOwner(models.Model):
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name="owners")
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owned_brands")
-    image = models.CharField(max_length=200, blank=False, default='')
-    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+    updated = models.DateTimeField(auto_now_add=True, null=True)
+
+class BrandFollower(models.Model):
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name="followers")
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following_brands")
     created = models.DateTimeField(auto_now_add=True, null=True)
     updated = models.DateTimeField(auto_now_add=True, null=True)
