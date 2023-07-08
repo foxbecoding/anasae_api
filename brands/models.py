ba6eb7e60d6912b1e83e3ae337b7fc31056c134d
FoxBeCoding <fox@foxbecoding.com>
2023-07-07 22:37:05 -0500

    updated models

--- brands/models.py
+++ brands/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from users.models import User
 
 class Brand(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="brand")
+    owners = models.ManyToManyField(User, related_name="brands")
     uid = models.CharField(max_length=20, blank=False, unique=True)
     name = models.CharField(max_length=200, blank=False)
     bio = models.CharField(max_length=2000, blank=False)
@@ -21,7 +21,7 @@ class BrandLogo(models.Model):
     deleted = models.DateTimeField(null=True)
 
 class BrandOwner(models.Model):
-    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name="owners")
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name="ownerss")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owned_brands")
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True, null=True)
