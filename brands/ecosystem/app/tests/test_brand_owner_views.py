edf36ea7901c7391ad44841de6ea741718b16dca
FoxBeCoding <fox@foxbecoding.com>
2023-07-08 21:14:51 -0500

    updated test

---
+++ brands/ecosystem/app/tests/test_brand_owner_views.py
@@ -0,0 +1,94 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from users.models import UserGender
+from datetime import datetime
+
+is_CSRF = True
+
+class TestBrandOwnerViewSet(TestCase):
+ 
+    def setUp(self):
+        self.client = Client(enforce_csrf_checks=is_CSRF)
+        self.client.get(reverse('x-fct-list'))
+        self.csrftoken = self.client.cookies['csrftoken'].value
+        self.User_Gender_Instance = UserGender.objects.create(gender = 'Male')
+        self.User_Gender_Instance.save()
+
+        date_time_str = '12/31/1990'
+        self.date_time_obj = datetime.strptime(date_time_str, '%m/%d/%Y')
+
+        user_data = {
+            'first_name': "Desmond",
+            'last_name': 'Fox',
+            'email': 'slugga@gmail.com',
+            'username': 'slugga',
+            'password': '123456',
+            'confirm_password': '123456',
+            'date_of_birth': self.date_time_obj.date(),
+            'agreed_to_toa': True,
+            'gender': self.User_Gender_Instance.id
+        }
+
+        self.client.post(
+            reverse('user-list'), 
+            user_data, 
+            **{'HTTP_X_CSRFTOKEN': self.csrftoken}
+        )
+
+        login_credentials = {
+            'username': 'slugga',
+            'password': '123456'
+        }
+    
+        login_res = self.client.post(
+            reverse('auth-log-in-list'), 
+            login_credentials, 
+            **{'HTTP_X_CSRFTOKEN': self.csrftoken}
+        )
+        self.user = login_res.data
+        self.csrftoken = self.client.cookies['csrftoken'].value
+
+        brand_request_data = { 
+            'name': 'Fenty Beauty',
+            'bio': 'Fenty Beauty by Rihanna was created with promise of inclusion for all women. With an unmatched offering of shades and colors for ALL skin tones, you&#39;ll never look elsewhere for your beauty staples. Browse our foundation line, lip colors, and so much more.'
+        }
+
+        brand_res = self.client.post(
+            reverse('brand-list'), 
+            data=brand_request_data, 
+            **{'HTTP_X_CSRFTOKEN': self.csrftoken}
+        ) 
+        self.brand_data = brand_res.data
+
+    def test_brand_owner_create(self):
+        owner_data = {
+            'first_name': "Rihhana",
+            'last_name': 'Fenty',
+            'email': 'riri@fentybeauty.com',
+            'username': 'badgalriri',
+            'password': '123456',
+            'confirm_password': '123456',
+            'date_of_birth': self.date_time_obj.date(),
+            'agreed_to_toa': True,
+            'gender': self.User_Gender_Instance.id
+        }
+
+        owner_res = self.client.post(
+            reverse('user-list'), 
+            owner_data, 
+            **{'HTTP_X_CSRFTOKEN': self.csrftoken}
+        )
+        print(owner_res)
+        # request_data = {
+        #     'brand': self.brand_data['pk'],
+        #     'user': 
+        # }
+
+        # res = self.client.post(
+        #     reverse('brand-owner-list'), 
+        #     data=request_data, 
+        #     **{'HTTP_X_CSRFTOKEN': self.csrftoken}
+        # )
+
+        # self.assertNotEqual(res.data['logo'], None)
+        # self.assertEqual(res.status_code, 201)   
