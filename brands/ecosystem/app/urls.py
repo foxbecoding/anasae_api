8022fee6e17386c26cce6cb99c4c7d1c249be4c2
FoxBeCoding <fox@foxbecoding.com>
2023-07-08 21:04:55 -0500

    updated views

--- brands/ecosystem/app/urls.py
+++ brands/ecosystem/app/urls.py
@@ -4,4 +4,5 @@ from brands.ecosystem.app.views import *
 router = DefaultRouter()
 router.register(r"brand", BrandViewSet, basename="brand")
 router.register(r"brand-logo", BrandLogoViewSet, basename="brand-logo")
+router.register(r"brand-owner", BrandOwnerViewSet, basename="brand-owner")
 urlpatterns = router.urls
