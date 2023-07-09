8f6c2d968d09b896be92d4d3d862f80583e4cfbf
FoxBeCoding <fox@foxbecoding.com>
2023-07-08 21:08:23 -0500

    updated test

--- brands/ecosystem/app/tests/test_app_urls.py
+++ brands/ecosystem/app/tests/test_app_urls.py
@@ -16,3 +16,13 @@ class TestBrandLogoUrls(SimpleTestCase):
     def test_brand_logo_list_url_resolves(self):
         url = reverse('brand-logo-list')
         self.assertEqual(resolve(url).view_name, 'brand-logo-list')
+
+class TestBrandOwnerUrls(SimpleTestCase):
+    
+    def test_brand_owner_list_url_resolves(self):
+        url = reverse('brand-owner-list')
+        self.assertEqual(resolve(url).view_name, 'brand-owner-list')
+
+    def test_brand_owner_detail_url_resolves(self):
+        url = reverse('brand-owner-detail', kwargs={'pk': 1})
+        self.assertEqual(resolve(url).view_name, 'brand-owner-detail')
