c92d7acdbe91e4f84928f27eb23e3d9997eb70b8
FoxBeCoding <fox@foxbecoding.com>
2023-07-09 16:12:58 -0500

    updated test

--- brands/ecosystem/app/tests/test_brand_follower_views.py
+++ brands/ecosystem/app/tests/test_brand_follower_views.py
@@ -103,7 +103,5 @@ class TestBrandFollowerViewSet(TestCase):
             data=request_data, 
             **{'HTTP_X_CSRFTOKEN': self.csrftoken}
         )
-
-        print(res.data)
-
+        
         self.assertEqual(res.status_code, 400)   
