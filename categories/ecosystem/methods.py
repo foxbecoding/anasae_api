5b0fa357a3945abde2da3c70d17df10d32268d99
FoxBeCoding <fox@foxbecoding.com>
2023-07-10 22:32:12 -0500

    updated methods

--- categories/ecosystem/methods.py
+++ categories/ecosystem/methods.py
@@ -1,3 +1,21 @@
 from categories.models import *
+from utils.helpers import create_uid
 
-def test_categories(): return 'Fox'
+def test_categories(): 
+    Category_Instance = Category.objects.create(
+        uid = create_uid('cat-'),
+        title = "Men's fashion",
+        description = "Shop men's fashion from the best Black Brands.",
+        is_active = True
+    )
+    Category_Instance.save()
+
+    Subcategory_Instance = Subcategory.objects.create(
+        category = Category_Instance,
+        uid = create_uid('scat-'),
+        title = 'Bottoms',
+        description = 'Best bottoms for men from black brands',
+        is_active = True
+    )
+    Subcategory_Instance.save()
+    
