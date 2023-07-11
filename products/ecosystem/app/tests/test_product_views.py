371fdac3b728f9f53e7748527834c3a8e90f2063
FoxBeCoding <fox@foxbecoding.com>
2023-07-11 18:38:08 -0500

    updated test

--- products/ecosystem/app/tests/test_product_views.py
+++ products/ecosystem/app/tests/test_product_views.py
@@ -1,8 +1,9 @@
 from django.test import TestCase, Client
 from django.urls import reverse
-from products.models import Product
+from users.models import UserGender
 from categories.ecosystem.methods import test_categories
 from pprint import pprint
+from datetime import datetime
 
 is_CSRF = True
 
@@ -10,7 +11,67 @@ class TestProductViewSet(TestCase):
  
     def setUp(self):
         self.categories = test_categories()
+        self.client = Client(enforce_csrf_checks=is_CSRF)
+        self.client.get(reverse('x-fct-list'))
+        self.csrftoken = self.client.cookies['csrftoken'].value
+        self.User_Gender_Instance = UserGender.objects.create(gender = 'Male')
+        self.User_Gender_Instance.save()
+
+        date_time_str = '12/31/1990'
+        date_time_obj = datetime.strptime(date_time_str, '%m/%d/%Y')
+
+        user_data = {
+            'first_name': "Desmond",
+            'last_name': 'Fox',
+            'email': 'slugga@gmail.com',
+            'username': 'slugga',
+            'password': '123456',
+            'confirm_password': '123456',
+            'date_of_birth': date_time_obj.date(),
+            'agreed_to_toa': True,
+            'gender': self.User_Gender_Instance.id
+        }
+
+        self.client.post(
+            reverse('user-list'), 
+            user_data, 
+            **{'HTTP_X_CSRFTOKEN': self.csrftoken}
+        )
+
+        login_credentials = {
+            'username': 'slugga',
+            'password': '123456'
+        }
+    
+        login_res = self.client.post(
+            reverse('auth-log-in-list'), 
+            login_credentials, 
+            **{'HTTP_X_CSRFTOKEN': self.csrftoken}
+        )
+        self.user = login_res.data
+        self.csrftoken = self.client.cookies['csrftoken'].value
+
+        brand_request_data = { 
+            'name': 'ANASAE',
+            'bio': 'ANASAE has all of the essentials for all of your needs.  Shop with us today!',
+        }
+
+        brand_res = self.client.post(
+            reverse('brand-list'), 
+            data=brand_request_data, 
+            **{'HTTP_X_CSRFTOKEN': self.csrftoken}
+        ) 
+        self.brand_data = brand_res.data
 
     def test_product_list(self):
-        self.categories
-        pprint(self.categories)
+        request_data = {
+            'brand': self.brand_data['pk'],
+            'category': self.categories['category_data']['pk'],
+            'subcategory': self.categories['subcategory_data']['pk'],
+        }
+        
+        res = self.client.post(
+            reverse('product-list'), 
+            data=request_data, 
+            **{'HTTP_X_CSRFTOKEN': self.csrftoken}
+        ) 
