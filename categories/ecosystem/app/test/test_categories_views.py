f9addacb389a7ab81b6776d37ef88ae6a1bada6e
FoxBeCoding <fox@foxbecoding.com>
2023-07-10 22:08:08 -0500

    updated test

---
+++ categories/ecosystem/app/test/test_categories_views.py
@@ -0,0 +1,12 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+
+is_CSRF = True
+
+class TestCategoryViewSet(TestCase):
+ 
+    def setUp(self):
+        pass
+
+    def test_category_list(self):
+        pass
