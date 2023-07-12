a8fd6ab66f62ba009105aa814d3dd64885b681e4
FoxBeCoding <fox@foxbecoding.com>
2023-07-12 11:11:42 -0500

    updated views

--- products/ecosystem/app/views.py
+++ products/ecosystem/app/views.py
@@ -7,6 +7,7 @@ from rest_framework.permissions import IsAuthenticated
 from products.models import *
 from products.serializers import *
 from products.permissions import *
+from products.ecosystem.methods import get_product_data
 from categories.ecosystem.methods import *
 from pprint import pprint
 
@@ -23,6 +24,6 @@ class ProductViewSet(viewsets.ViewSet):
         self.check_object_permissions(request=request, obj={'brand_pk': request.data['brand']})
         Create_Product_Serializer = CreateProductSerializer(data=request.data)
         if not Create_Product_Serializer.is_valid(): return False
-        data = ProductSerializer(Create_Product_Serializer.validated_data['product']).data
-        print(data)
+        data = ProductSerializer(Create_Product_Serializer.validated_data['product'])
+        get_product_data((str(data.id)))
         return Response(None, status=status.HTTP_200_OK)
