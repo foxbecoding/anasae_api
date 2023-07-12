b5c5fd8e3ffc542d18acc4372e052ce613d79a9f
FoxBeCoding <fox@foxbecoding.com>
2023-07-12 18:53:12 -0500

    updated permissions

--- products/permissions.py
+++ products/permissions.py
@@ -9,20 +9,20 @@ class ProductPermission(BasePermission):
     message = "Access Denied!"   
 
     def has_object_permission(self, request, view, obj) -> bool:
+        brand_pk = ''
         if request.method == 'POST':
             if 'brand' not in request.data: return False 
             brand_pk = str(obj['brand_pk'])
-            Brand_Owner_Instances = BrandOwner.objects.filter(user_id=str(request.user.id))
-            brand_owner_data = BrandOwnerSerializer(Brand_Owner_Instances, many=True).data
-            brand_pks = [ str(brand['brand']) for brand in brand_owner_data ]
-            if brand_pk not in brand_pks: return False
         
         if request.method == 'PUT':
             product_pk = obj['product_pk']
             if not Product.objects.filter(pk=product_pk).exists(): return False
             Product_Instance = Product.objects.get(pk=product_pk)
             product_data = ProductSerializer(Product_Instance).data
-            pro_brand_pk = product_data['brand']
-            print(pro_brand_pk)
-            Brand_Owner_Instances = BrandOwner.objects.filter(user_id=str(request.user.id))
+            brand_pk = str(product_data['brand'])
+        
+        Brand_Owner_Instances = BrandOwner.objects.filter(user_id=str(request.user.id))
+        brand_owner_data = BrandOwnerSerializer(Brand_Owner_Instances, many=True).data
+        brand_pks = [ str(brand['brand']) for brand in brand_owner_data ]
+        if brand_pk not in brand_pks: return False
         return True
