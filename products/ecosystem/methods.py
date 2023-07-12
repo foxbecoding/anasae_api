0fa0e4d634a92cb4fa1c47e22af04328d44ad6e7
FoxBeCoding <fox@foxbecoding.com>
2023-07-12 13:25:53 -0500

    updated methods

--- products/ecosystem/methods.py
+++ products/ecosystem/methods.py
@@ -4,24 +4,13 @@ from categories.models import Category, Subcategory
 from categories.serializers import CategorySerializer, SubcategorySerializer
 from products.models import *
 from products.serializers import *
-
-def filter_data(obj, filter = []):
-    if len(filter) == 0 or len(obj) == 0: return obj
-    newDict = dict()
-    for (key, value) in obj.items():
-        if key in filter:
-            newDict[key] = value
-    return newDict
+from utils.helpers import filter_obj
 
 def unzip_products(zip):
-    product = zip[0]
-    brand = zip[1]
-    category = zip[2]
-    subcategory = zip[3]
-
-    product['brand'] = filter_data(brand, filter = ['pk','uid','name','logo'])
-    product['category'] = filter_data(category, filter = ['pk','uid','title'])
-    product['subcategory'] = filter_data(subcategory, filter = ['pk','uid','title'])
+    product, brand, category, subcategory = zip
+    product['brand'] = filter_obj(brand, filter = ['pk','uid','name','logo'])
+    product['category'] = filter_obj(category, filter = ['pk','uid','title'])
+    product['subcategory'] = filter_obj(subcategory, filter = ['pk','uid','title'])
     return product
 
 def get_product_rel_data(product_data, key, model, serializer):
