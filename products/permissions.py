9d240a98e55c196417654811cbdb80a18cd51511
FoxBeCoding <fox@foxbecoding.com>
2023-07-12 18:19:22 -0500

    updated permissions

--- products/permissions.py
+++ products/permissions.py
@@ -9,13 +9,15 @@ class ProductPermission(BasePermission):
     message = "Access Denied!"   
 
     def has_object_permission(self, request, view, obj) -> bool:
-        if 'brand' not in request.data: return False 
-    
-        brand_pk = str(obj['brand_pk'])
-        Brand_Owner_Instances = BrandOwner.objects.filter(user_id=str(request.user.id))
-        brand_owner_data = BrandOwnerSerializer(Brand_Owner_Instances, many=True).data
-        brand_pks = [ str(brand['brand']) for brand in brand_owner_data ]
+        if request.method == 'POST':
+            if 'brand' not in request.data: return False 
+            brand_pk = str(obj['brand_pk'])
+            Brand_Owner_Instances = BrandOwner.objects.filter(user_id=str(request.user.id))
+            brand_owner_data = BrandOwnerSerializer(Brand_Owner_Instances, many=True).data
+            brand_pks = [ str(brand['brand']) for brand in brand_owner_data ]
+            if brand_pk not in brand_pks: return False
         
-        if brand_pk not in brand_pks: return False
-        
+        if request.method == 'GET':
+            product_pk = obj['product_pk']
+            if not Product.objects.filter(pk=product_pk).exists(): return False
         return True
