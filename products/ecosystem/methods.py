9e5a00852619300f1055258e16f6635706b1f642
FoxBeCoding <fox@foxbecoding.com>
2023-07-12 12:30:10 -0500

    updated method

--- products/ecosystem/methods.py
+++ products/ecosystem/methods.py
@@ -3,6 +3,22 @@ from products.serializers import *
 from brands.models import Brand
 from brands.serializers import BrandSerializer
 
+def filter_brand_data(obj):
+    return {
+        'pk': obj['pk'],
+        'uid': obj['uid'],
+        'name': obj['name'],
+        'logo': obj['logo']
+    }
+
+def unzip_products(zip):
+    product = zip[0]
+    brand = zip[1]
+
+    product['brand'] = filter_brand_data(brand)
+
+    return product
+
 def get_product_data(pks = (), many = False):
     Product_Instances = Product.objects.filter(pk__in=pks)
     product_data = ProductSerializer(Product_Instances, many=True).data
@@ -11,8 +27,8 @@ def get_product_data(pks = (), many = False):
     Brand_Instances = Brand.objects.filter(pk__in=brand_pks)
     brand_data = BrandSerializer(Brand_Instances, many=True).data
 
-    product = list(zip(product_data, brand_data))
-    print(product)
+    products_zip = list(zip(product_data, brand_data))
+    products_unzip = [ unzip_products(zip) for zip in products_zip ]
 
     return product_data if many else product_data[0]
     
