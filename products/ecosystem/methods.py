1210d9b3e0cbe6ffaa38d26569eadd90b55bcaa3
FoxBeCoding <fox@foxbecoding.com>
2023-07-12 12:41:50 -0500

    updated methods

--- products/ecosystem/methods.py
+++ products/ecosystem/methods.py
@@ -1,7 +1,9 @@
-from products.models import *
-from products.serializers import *
 from brands.models import Brand
 from brands.serializers import BrandSerializer
+from categories.models import Category, Subcategory
+from categories.serializers import CategorySerializer, SubcategorySerializer
+from products.models import *
+from products.serializers import *
 
 def filter_brand_data(obj):
     return {
@@ -26,9 +28,13 @@ def get_product_data(pks = (), many = False):
     brand_pks = tuple( str(data['brand']) for data in product_data )
     Brand_Instances = Brand.objects.filter(pk__in=brand_pks)
     brand_data = BrandSerializer(Brand_Instances, many=True).data
+    
+    category_pks = tuple( str(data['category']) for data in product_data )
+    Category_Instances = Category.objects.filter(pk__in=category_pks)
+    category_data = CategorySerializer(Category_Instances, many=True).data
 
     products_zip = list(zip(product_data, brand_data))
-    products_unzip = [ unzip_products(zip) for zip in products_zip ]
-
+    products = [ unzip_products(zip) for zip in products_zip ]
+    print(products)
     return product_data if many else product_data[0]
     
