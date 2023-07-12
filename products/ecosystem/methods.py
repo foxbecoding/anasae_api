6d3738b0730c8562f49dde36d21cbdf250ed4daf
FoxBeCoding <fox@foxbecoding.com>
2023-07-12 11:08:47 -0500

    updated methods

---
+++ products/ecosystem/methods.py
@@ -0,0 +1,8 @@
+from products.models import *
+from products.serializers import *
+
+def get_product_data(pks = (), many = False):
+    Product_Instances = Product.objects.fitler(pk__in=pks)
+    product_data = ProductSerializer(Product_Instances, many=True).data
+
+    return product_data if many else product_data[0]
