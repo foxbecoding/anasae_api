1acd4f97933624136ce35b6127c55a830ad2aeac
FoxBeCoding <fox@foxbecoding.com>
2023-07-12 13:03:21 -0500

    updated methods

--- products/ecosystem/methods.py
+++ products/ecosystem/methods.py
@@ -16,24 +16,28 @@ def filter_brand_data(obj):
 def unzip_products(zip):
     product = zip[0]
     brand = zip[1]
+    category = zip[2]
 
     product['brand'] = filter_brand_data(brand)
-
+    product['category'] = category
     return product
+
+def get_product_rel_data(product_data, key, model, serializer):
+    pks = tuple( str(data[key]) for data in product_data )
+    instances = model.objects.filter(pk__in=pks)
+    return serializer(instances, many=True).data
 
 def get_product_data(pks = (), many = False):
     Product_Instances = Product.objects.filter(pk__in=pks)
     product_data = ProductSerializer(Product_Instances, many=True).data
+    brand_data = get_product_rel_data(product_data, 'brand', Brand, BrandSerializer)
     
-    brand_pks = tuple( str(data['brand']) for data in product_data )
-    Brand_Instances = Brand.objects.filter(pk__in=brand_pks)
-    brand_data = BrandSerializer(Brand_Instances, many=True).data
-    
-    category_pks = tuple( str(data['category']) for data in product_data )
-    Category_Instances = Category.objects.filter(pk__in=category_pks)
-    category_data = CategorySerializer(Category_Instances, many=True).data
+    # category_pks = tuple( str(data['category']) for data in product_data )
+    # Category_Instances = Category.objects.filter(pk__in=category_pks)
+    # category_data = CategorySerializer(Category_Instances, many=True).data
+    category_data = get_product_rel_data(product_data, 'category', Category, CategorySerializer)
 
-    products_zip = list(zip(product_data, brand_data))
+    products_zip = list(zip(product_data, brand_data, category_data))
     products = [ unzip_products(zip) for zip in products_zip ]
     print(products)
     return product_data if many else product_data[0]
