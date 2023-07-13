7f48fa3c723478030103fc02be514acece7085b7
FoxBeCoding <fox@foxbecoding.com>
2023-07-13 13:55:57 -0500

    updated serializers

--- products/serializers.py
+++ products/serializers.py
@@ -50,9 +50,10 @@ class CreateProductSerializer(serializers.ModelSerializer):
         ]
 
     def validate(self, attrs):
+        group_id = self.context['request'].data['group_id']
         Product_Instance = Product.objects.create(
             uid = create_uid('pro-'),
-            group_id = attrs.get('sku') or None,
+            group_id = group_id or None,
             brand = attrs.get('brand'),
             category = attrs.get('category'),
             subcategory = attrs.get('subcategory'),
