01ca0e21ce2a79190f1323b22ead4e844a990c88
FoxBeCoding <fox@foxbecoding.com>
2023-07-14 21:31:05 -0500

    updated classes, test

--- products/ecosystem/classes.py
+++ products/ecosystem/classes.py
@@ -19,23 +19,27 @@ class ProductData:
         category = self.__get_product_rel_data(products_data, 'category', Category, CategorySerializer)
         subcategory = self.__get_product_rel_data(products_data, 'subcategory', Subcategory, SubcategorySerializer)
         price = self.__get_product_rel_data(products_data, 'price', ProductPrice, ProductPriceSerializer)
-        products_zip = tuple( zip(products_data, brand, category, subcategory, price) )
+        specifications = self.__get_product_rel_data(products_data, 'specifications', ProductSpecification, ProductSpecificationsSerializer)
+        # print(specifications)
+        products_zip = tuple( zip(products_data, brand, category, subcategory, price, [specifications]) )
         products = tuple( self.__unzip_products(zip) for zip in products_zip )
         self.products = products if self.many else products[0]
 
     def __get_product_rel_data(self, product_data, key, model, serializer):
         pks = tuple( data[key] for data in product_data )
+        if key == 'specifications': pks = pks[0]
         if not model.objects.filter(pk__in=pks).exists(): return [{}]
         instances = model.objects.filter(pk__in=pks)
         return serializer(instances, many=True).data
 
     def __unzip_products(self, zip):
-        product, brand, category, subcategory, price = zip
+        product, brand, category, subcategory, price, specifications = zip
         product_rel_data = (
             {'data': brand, 'key': 'brand', 'filter': ['pk','uid','name','logo']},
             {'data': price, 'key': 'price', 'filter': ['price']},
             {'data': category, 'key': 'category', 'filter': ['pk','uid','title']},
-            {'data': subcategory, 'key': 'subcategory', 'filter': ['pk','uid','title']}
+            {'data': subcategory, 'key': 'subcategory', 'filter': ['pk','uid','title']},
+            {'data': specifications, 'key': 'specifications', 'filter': ['pk','label','value','is_required']}
         )
         for data in product_rel_data:
             obj = filter_obj(data['data'], data['filter']) 
