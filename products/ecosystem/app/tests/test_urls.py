6a7ca036e70c5d249761abc2c5b9e09dae86af63
FoxBeCoding <fox@foxbecoding.com>
2023-07-16 13:37:51 -0500

    updated test

--- products/ecosystem/app/tests/test_urls.py
+++ products/ecosystem/app/tests/test_urls.py
@@ -10,3 +10,13 @@ class TestProductUrls(SimpleTestCase):
     def test_product_detail_url_resolves(self):
         url = reverse('product-detail', kwargs={'pk': 1})
         self.assertEqual(resolve(url).view_name, 'product-detail')
+
+class TestProductPriceUrls(SimpleTestCase):
+    
+    def test_product_price_list_url_resolves(self):
+        url = reverse('product-price-list')
+        self.assertEqual(resolve(url).view_name, 'product-price-list')
+    
+    def test_product_price_detail_url_resolves(self):
+        url = reverse('product-price-detail', kwargs={'pk': 1})
+        self.assertEqual(resolve(url).view_name, 'product-price-detail')
