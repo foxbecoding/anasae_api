fbee6269fa132c6b05cefa01d328fc95f364ce7a
FoxBeCoding <fox@foxbecoding.com>
2023-07-16 13:35:33 -0500

    updated test

--- products/ecosystem/app/tests/test_product_views.py
+++ products/ecosystem/app/tests/test_product_views.py
@@ -200,28 +200,3 @@ class TestProductViewSet(TestCase):
         ) 
         
         self.assertEqual(res.status_code, 400)
-
-
-    # spec_values = data['spec_values']
-    #         product_specs = []
-    #         if product['category'] and product['subcategory']:
-    #             specifications = self.categories['subcategory_data']['product_specification']
-    #             for spec in list(zip(specifications, spec_values)):
-    #                 spec_data, spec_value = spec[0], spec[1]
-    #                 product_specs.append({
-    #                     'label': spec_data['item'],
-    #                     'is_required': spec_data['is_required'],
-    #                     'value': spec_value,
-    #                 })
-    #         else:
-    #             specifications = self.categories['category']['product_specification']
-    #             for spec in list(zip(specifications, spec_values)):
-    #                 spec_data, spec_value = spec[0], spec[1]
-    #                 product_specs.append({
-    #                     'label': spec_data['item'],
-    #                     'is_required': spec_data['is_required'],
-    #                     'value': spec_value,
-    #                 })
-
-    #         product['specifications'] = product_specs
-    #         product['images'] = [tmp_image() for x in range(7)]
