99e9471a08de644d21270204598fa64b03078ecd
FoxBeCoding <fox@foxbecoding.com>
2023-07-17 14:58:17 -0500

    updated urls

--- products/ecosystem/app/urls.py
+++ products/ecosystem/app/urls.py
@@ -4,4 +4,5 @@ from products.ecosystem.app.views import *
 router = DefaultRouter()
 router.register(r"product", ProductViewSet, basename="product")
 router.register(r"product-price", ProductPriceViewSet, basename="product-price")
+router.register(r"product-specification", ProductSpecificationViewSet, basename="product-specification")
 urlpatterns = router.urls
