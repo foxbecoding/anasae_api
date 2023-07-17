1898c9a0a9f19a632fa710013a6e76b19e12844e
FoxBeCoding <fox@foxbecoding.com>
2023-07-17 14:59:56 -0500

    updated test

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,4 @@
 # from products.ecosystem.app.tests.test_urls import TestProductUrls
 # from products.ecosystem.app.tests.test_product_views import TestProductViewSet
-from products.ecosystem.app.tests.test_product_price_views import TestProductPriceViewSet
+# from products.ecosystem.app.tests.test_product_price_views import TestProductPriceViewSet
+from products.ecosystem.app.tests.test_product_specifications_views import TestProductSpecificationViewSet
