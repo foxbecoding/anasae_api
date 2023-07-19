5ec87deef227de610f6ce43a85cf33d7e53c24e6
FoxBeCoding <fox@foxbecoding.com>
2023-07-19 16:09:21 -0500

    updated views

--- products/ecosystem/app/views.py
+++ products/ecosystem/app/views.py
@@ -97,6 +97,5 @@ class ProductSpecificationViewSet(viewsets.ViewSet):
         instances = ProductSpecification.objects.filter(pk__in=pks)
         edit_serializer = EditProductSpecificationSerializer(data=request.data, many=True)
         if not edit_serializer.is_valid(): return Response(edit_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        BulkEditProductSpecificationSerializer(instances, edit_serializer.validated_data)
-        # return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
-        return Response(None, status=status.HTTP_202_ACCEPTED)
+        data = BulkEditProductSpecificationSerializer(instances, edit_serializer.validated_data).specifications
+        return Response(data, status=status.HTTP_202_ACCEPTED)
