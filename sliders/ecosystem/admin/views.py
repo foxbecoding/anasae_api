ef773fac853350063dfb5d842d5b02867ecfde7c
FoxBeCoding <fox@foxbecoding.com>
2023-07-25 18:37:07 -0500

    updated views

--- sliders/ecosystem/admin/views.py
+++ sliders/ecosystem/admin/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_protect
+from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+from pprint import pprint
 
-# Create your views here.
+class SliderViewSet(viewsets.ViewSet):
+    def get_permissions(self):
+        permission_classes = [IsAuthenticated, IsAdminUser]
+        return [permission() for permission in permission_classes]
+
+    def create(self, request):
+        print('Slider')
+        return Response(None, status=status.HTTP_200_OK)
+    
+class SliderImageViewSet(viewsets.ViewSet):
+    def get_permissions(self):
+        permission_classes = [IsAuthenticated, IsAdminUser]
+        return [permission() for permission in permission_classes]
+
+    def create(self, request):
+        print('SliderImage')
+        return Response(None, status=status.HTTP_200_OK)
