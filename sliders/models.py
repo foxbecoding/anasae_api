5caaeb7dcc0873f04e38ecb527a1b04e9f1640fd
FoxBeCoding <fox@foxbecoding.com>
2023-07-25 18:29:28 -0500

    updated sliders app

--- sliders/models.py
+++ sliders/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Slider(models.Model):
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+    updated = models.DateTimeField(auto_now_add=True, null=True)
+
+class SliderImage(models.Model):
+    slider = models.ForeignKey(Slider, on_delete=models.CASCADE)
+    image = models.CharField(max_length=200, blank=False, null=False)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+    updated = models.DateTimeField(auto_now_add=True, null=True)
