94c59d4c3a98737f255235d4d940315c83b1b45c
FoxBeCoding <fox@foxbecoding.com>
2023-07-25 19:32:53 -0500

    updated serializers

---
+++ sliders/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from sliders.models import Slider, SliderImage
+from utils.helpers import create_uid
+from PIL import Image
+import requests, os, calendar, time
+
+env = os.getenv
+
+class SliderSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Slider
+        fields = [
+            'pk',
+            'name',
+            'is_active'
+        ]
+
+class CreateSliderSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Slider
+        fields = [ 'name' ]
