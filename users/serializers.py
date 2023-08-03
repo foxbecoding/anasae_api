6c41270e7c5754cd9ae8a618ffb86f6a4e1912c0
FoxBeCoding <fox@foxbecoding.com>
2023-08-03 13:38:48 -0500

    updated serializers

--- users/serializers.py
+++ users/serializers.py
@@ -239,19 +239,19 @@ class UserAuthValidateDetailsSerializer(serializers.ModelSerializer):
             'date_of_birth'
         ]
 
-    def validate(self, attrs):
-        username = attrs.get('username').lower()
-        email = attrs.get('email').lower()
+    # def validate(self, attrs):
+    #     username = attrs.get('username').lower()
+    #     email = attrs.get('email').lower()
        
-        if User.objects.filter(username=username).exists():
-            msg = 'user with this username already exists'
-            raise serializers.ValidationError({'username': msg}, code='authorization')
+    #     if User.objects.filter(username=username).exists():
+    #         msg = 'user with this username already exists'
+    #         raise serializers.ValidationError({'username': msg}, code='authorization')
 
-        if User.objects.filter(email=email).exists():
-            msg = 'user with this email already exists.'
-            raise serializers.ValidationError({'email': msg}, code='authorization')
+    #     if User.objects.filter(email=email).exists():
+    #         msg = 'user with this email already exists.'
+    #         raise serializers.ValidationError({'email': msg}, code='authorization')
         
-        return attrs
+    #     return attrs
         
 class UserLoginSerializer(serializers.ModelSerializer):
     class Meta:
