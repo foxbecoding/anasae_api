3b275bc2d8c4d07b235d8d40ef2209c65f0f64d2
FoxBeCoding <fox@foxbecoding.com>
2023-08-29 11:10:17 -0500

    updated methods

--- users/ecosystem/methods.py
+++ users/ecosystem/methods.py
@@ -20,6 +20,7 @@ def get_user_data(instance: User, filter = []):
         'display_name': user_data['display_name'],
         'email': user_data['email'],
         'first_name': user_data['first_name'],
+        'followed_users': len(user_data['followed_users']),
         'followers': len(user_data['followers']),
         'image': user_data['image'],
         'last_name': user_data['last_name'],
